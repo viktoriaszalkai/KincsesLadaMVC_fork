e9426fa1e2a2ec738413433f7e5ace7554c317a8
szalkai.v.csenge <szalkai.v.csenge@host.docker.internal>
2024-10-07 16:14:35 +0200

    ora vege

--- src/vezerlo/GuiVezerlo.java
+++ src/vezerlo/GuiVezerlo.java
@@ -1,9 +1,9 @@
-
 package vezerlo;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import modell.JatekModell;
 import nezet.GuiNezet;
-
 
 public class GuiVezerlo {
 
@@ -14,27 +14,33 @@ public class GuiVezerlo {
         this.modell = modell;
         this.nezet = nezet;
         guiBeallitas();
-        start(); 
+        start();
         valasztas();
     }
-    private void start(){
+
+    private void start() {
         nezet.megjelenit(modell.kezdes());
-    }  
-    
-    private void valasztas(){
-        modell.setValasztas(nezet.bekeres());
-        int v;
-        v = nezet.bekeres();
-        modell.setValasztas(v);
-        if( v > -1){
-            nezet.visszajelzes(modell.ellenorzes());    
-        }else{
-            nezet.visszajelzes("Nincs láda kiválasztva");
-        }
     }
-    
-    private void guiBeallitas(){
-    java.awt.EventQueue.invokeLater(new Runnable() {
+
+    private void valasztas() {
+        //modell.setValasztas(nezet.bekeres());
+        JButton gomb = nezet.getBtnValasztas();
+        gomb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int v;
+                v = nezet.bekeres();
+                modell.setValasztas(v);
+                String s = "Nincs láda kiválasztva!";
+                s = v > -1 ? modell.ellenorzes() : s;
+                nezet.visszajelzes(s);
+            }
+        });
+
+    }
+
+    private void guiBeallitas() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 nezet.setVisible(true);
             }
